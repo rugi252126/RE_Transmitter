773be4cb40f4fa0badf1332150ba9a6ddf40dd79
Rudy Alfonso <alfonso_rudy@yahoo.com>
2019-11-07 22:13:39 +0800

    SWQ: Code clean-up and added meaningful comments

--- lib/RE_Menu/RE_Menu.cpp
+++ lib/RE_Menu/RE_Menu.cpp
@@ -1,90 +1,92 @@
-/*
- Add description
- */
+/**
+    @file RE_Menu.cpp
 
-//#include "RE_Rte.h"
+    @brief
+    This handles the different menu and browse menu in the system.
+    In main menu, user can choose what to control(e.g. robot, drone, etc..) and can set the controller mode.
+    Basically, it can set the controller configuration.
+    And in browse menu, user can see the actual settings of the controller and the status like
+    connection, battery, throttle, direction, etc..
+
+    @author Alfonso, Rudy Manalo
+    @version
+*/
+
 #include "RE_Menu.h"
-//#include "RE_Hal.h"
 
 
-/*
- */
+// CLASS CONSTRUCTORS
+// ---------------------------------------------------------------------------
 RE_Menu_cls::RE_Menu_cls()
 {
-
 }
 
-/*
- */
-bool RE_Menu_cls::menuF_getMainmenuStatus(void)
+// PUBLIC METHODS
+// ---------------------------------------------------------------------------
+// Module initialization
+void RE_Menu_cls::menuF_Init(void)
 {
-    return(main_menu_status_b);
+    // Every POR, initialize into default and desirable states
+    main_menu_e                            = MENU_MAIN_OPTIONS_E;
+    browse_menu_e                          = MENU_DISPLAY_GENERAL_INFO_E;
+    menu_switch_browse_state_e             = MENU_SWITCH_NOT_PRESSED_E;
+    main_menu_status_b                     = false;
+    main_menu_entry_exit_b                 = false;
+    browse_menu_status_b                   = false;
+    browse_menu_ctr_u8                     = 0u;
+    main_menu_ctr_u8                       = 0u;
+    main_menu_entry_exit_debounce_time_u16 = 0u;
 }
 
-/*
- */
-//enum browse_menu_et RE_Menu_cls::menuF_getBrowseMenu(void)
+// ---------------------------------------------------------------------------
+// Returns main menu status
+//bool RE_Menu_cls::menuF_getMainmenuStatus(void)
 //{
-//    return(browse_menu_e);
+//    return(main_menu_status_b);
 //}
 
-/*
- */
-//enum main_menu_et RE_Menu_cls::menuF_getMainMenu(void)
-//{
- //   return(main_menu_e);
-//}
-
-/*
- */
-//void RE_Menu_cls::menuF_RTE_Read_MenuSwitchState(bool state_b)
-//{
- //   RTE_Read_menu_switch_state_b = state_b;
-//}
-
-/*
- */
+// ---------------------------------------------------------------------------
+// Cyclic function
 void RE_Menu_cls::menuF_Cyclic(void)
 {
-    //! first, get the menu switch status
+    // first, get the menu switch status
     menuLF_checkMenuSwitchStatus();  
 
-    //! execute main menu or browse menu
+    // execute main menu or browse menu
     menuLF_browseMenu();
 
     //menuLF_mainMenu();
 
     // Update Rte data
     menuLF_updateRteData();
-
 }
 
-/***********************************************************************/
-
-/*
- */
+// PRIVATE METHODS
+// ---------------------------------------------------------------------------
+// Main menu entry/exit debounce time
 void RE_Menu_cls::menuLF_checkMainMenuDebounceTime(void)
 {
     if(main_menu_entry_exit_debounce_time_u16 >= MENU_MAIN_DEBOUNCE_TIME_K)
     {
-        if(main_menu_entry_exit_b != 1)
-        { // main menu entry/exit is triggered.
-            main_menu_entry_exit_b = 1u;
+        if(main_menu_entry_exit_b != true)
+        { 
+            // main menu entry/exit is triggered.
+            main_menu_entry_exit_b = true;
 
-            if(main_menu_status_b != 0)
+            if(main_menu_status_b != false)
             {
                 // main menu is de-activated. Can process the browse menu in this state.
-                main_menu_status_b = 0u;
+                main_menu_status_b = false;
             }
             else
             {
                 // main menu is activated. Can't process the browse menu in this state.
-                main_menu_status_b = 1u;
+                main_menu_status_b = true;
             }
         }
         else
         {
-            /* no action */
+            // no action
         }    
     }
     else
@@ -93,21 +95,22 @@ void RE_Menu_cls::menuLF_checkMainMenuDebounceTime(void)
     }
 }
 
-/*
- */
+// ---------------------------------------------------------------------------
+// Reading and checking main menu switch state
 void RE_Menu_cls::menuLF_checkMenuSwitchStatus(void)
 {
     bool tmp_b;
 
 
-    tmp_b = Rte_cls.Rte_Read_Hal_DI_MenuSwitch(); //Rte_cls.Rte_Read_SwitchIn_MenuSwitch(); //RTE_Read_menu_switch_state_b; // switchIn_cls.switchInF_getSwitchState(HAL_DI_MENU_SWITCH_E);
+    tmp_b = Rte_cls.Rte_Read_Hal_DI_MenuSwitch(); 
 
-    if(MENU_SWITCH_PRESSED_E == tmp_b)
+    if((bool)MENU_SWITCH_PRESSED_E == tmp_b)
     {
         menuLF_checkMainMenuDebounceTime();
 
-        if(main_menu_status_b != 1)
-        { // main menu is not activated. Possible to do menu browsing.
+        if(main_menu_status_b != true)
+        { 
+            // main menu is not activated. Possible to do menu browsing.
             menu_switch_browse_state_e = MENU_SWITCH_PRESSED_E;
         }
     }
@@ -115,40 +118,38 @@ void RE_Menu_cls::menuLF_checkMenuSwitchStatus(void)
     {
         menu_switch_browse_state_e = MENU_SWITCH_NOT_PRESSED_E;
 
-        if(main_menu_entry_exit_b != 0u)
+        if(main_menu_entry_exit_b != false)
         {
             // reset main menu entry/exit status
-            main_menu_entry_exit_b = 0u;
+            main_menu_entry_exit_b = false;
 
             // reset the counter
             main_menu_entry_exit_debounce_time_u16 = 0u;
         }
         else
         {
-            /* no action */
+            // no action
         }
-        
     }
-    
 }
 
-/*
- */
+// ---------------------------------------------------------------------------
+// Browse menu options checking and assignment
 void RE_Menu_cls::menuLF_browseMenu(void)
 {
     switch(menu_switch_browse_state_e)
     {
         case MENU_SWITCH_PRESSED_E:
         {
-            browse_menu_status_b = 1u;
+            browse_menu_status_b = true;
             break;
         }
         case MENU_SWITCH_NOT_PRESSED_E:
         {
-            if(browse_menu_status_b != 0u)
+            if(browse_menu_status_b != false)
             {
                 // reset the browse_menu_status_b and wait for new pressed
-                browse_menu_status_b = 0u;
+                browse_menu_status_b = false;
 
                 // menu switch is pressed and un-pressed. increment browse counter
                 browse_menu_ctr_u8++;
@@ -159,14 +160,14 @@ void RE_Menu_cls::menuLF_browseMenu(void)
                 }
                 else
                 {
-                    /* no action */
+                    // no action
                 }
                  
                 browse_menu_e = (enum browse_menu_et)browse_menu_ctr_u8;
             }
             else
             {
-                /* no action */
+                // no action
             }
             
             break;
@@ -178,18 +179,19 @@ void RE_Menu_cls::menuLF_browseMenu(void)
     }
 }
 
-/*
- */
-void RE_Menu_cls::menuLF_mainMenu(void)
-{
+// ---------------------------------------------------------------------------
+// Main menu checking and controller settings configuration
+//void RE_Menu_cls::menuLF_mainMenu(void)
+//{
+//}
 
-}
-
-/*
- */
+// ---------------------------------------------------------------------------
+// Update Rte data
 void RE_Menu_cls::menuLF_updateRteData(void)
 {
+    // Update Rte data for browse menu
     Rte_cls.Rte_Write_Menu_BrowseMenu(browse_menu_e);
 
+    // Update Rte data for main menu
     Rte_cls.Rte_Write_Menu_MainMenu(main_menu_e);
 }
--- lib/RE_Menu/RE_Menu.h
+++ lib/RE_Menu/RE_Menu.h
@@ -1,12 +1,43 @@
+/**
+    @file RE_Menu.h
+
+    @brief
+    This handles the different menu and browse menu in the system.
+    In main menu, user can choose what to control(e.g. robot, drone, etc..) and can set the controller mode.
+    Basically, it can set the controller configuration.
+    And in browse menu, user can see the actual settings of the controller and the status like
+    connection, battery, throttle, direction, etc..
+
+    @author Alfonso, Rudy Manalo
+    @version
+*/
+
 #ifndef RE_MENU_H_
 #define RE_MENU_H_
 
-//#include "Arduino.h"
 #include "RE_Rte.h"
 
+
+/*!
+    @defined 
+    @abstract Defines module cyclic time
+    @discussion This constant defines the module cyclic time.
+    The time is in milli-second form.
+*/
 #define MENU_CYCLIC_TIME_K            200u                      // 200ms
+
+/*!
+    @defined 
+    @abstract Time to enter main menu
+    @discussion When the menu switch is pressed continously within 3sec., the system will enter in main menu.
+*/
 #define MENU_MAIN_DEBOUNCE_TIME_K    (3000u/MENU_CYCLIC_TIME_K) // 3sec.
 
+/*!
+    @defined 
+    @abstract Switch state
+    @discussion The enumeration defines the different switch state for digital IO switch.
+*/
 enum menu_switch_state_et
 {
      MENU_SWITCH_PRESSED_E     = 0
@@ -14,86 +45,113 @@ enum menu_switch_state_et
     ,MENU_SWITCH_MAX_E
 };
 
-#if 0
-enum main_menu_et
-{
-     MENU_MAIN_OPTIONS_E
-    ,MENU_MAIN_MODE_E
-};
-
-enum browse_menu_et
-{
-     MENU_DISPLAY_GENERAL_INFO_E   = 0 // connection, controller mode
-    ,MENU_DISPLAY_VOLTAGE_INFO_E   = 1 // voltage and its status(normal, low voltage, critical low voltage)
-    ,MENU_DISPLAY_JOYSTICK_INFO_E  = 2 // throttle and control info
-    ,MENU_DISPLAY_SWITCH_INFO_E    = 3 // switch 1 to 4 info
-    ,MENU_DISPLAY_POTI_INFO_E      = 4 // potentiometer info
-    ,MENU_DISPLAY_CONTROLLER_TYPE_E= 5 // robot, drone, plane, boat, car, etc..(default: robot)
-    ,MENU_DISPLAY_MAX_E
-};
-#endif
 
 class RE_Menu_cls
 {
 public:
 
-    /*
-     */
+    /*!
+        @method
+        @abstract RE_Menu abstract constructor
+        @discussion RE_Menu class abstract constructor needed to create the abstract base class.
+    */
     RE_Menu_cls();
 
-    /*
-     */
-    bool menuF_getMainmenuStatus(void);
+    /*!
+        @function
+        @abstract Module initialization
+        @discussion It sets the initial state of the module.
 
-    /*
-     */
-    //enum browse_menu_et menuF_getBrowseMenu(void);
+        @param  none
+        @return none
+    */
+    void menuF_Init(void);       
 
-    /*
-     */
-    //enum main_menu_et menuF_getMainMenu(void);   
+    /*!
+        @function
+        @abstract Return main menu status
+        @Discussion Returns whether its inside the main menu or not.
 
-    /*
-     */
-    //void menuF_RTE_Read_MenuSwitchState(bool state_b);
+        @param  none
+        @return {[true/false] true=inside main menu; false=otherwise}
+    */
+    // bool menuF_getMainmenuStatus(void);
 
-    /*
-     */
+    /*!
+        @function
+        @abstract Module cyclic function
+        @discussion Tasks within the module that need to be called cyclically will be done here.
+
+        @param  none
+        @return none
+    */
     void menuF_Cyclic(void);
 
 protected:
     
-    enum main_menu_et main_menu_e;
-    enum browse_menu_et browse_menu_e;
-    enum menu_switch_state_et menu_switch_browse_state_e;
-    bool main_menu_status_b; // 1 = inside main menu; 0 = otherwise
-    bool main_menu_entry_exit_b;
-    bool browse_menu_status_b;
-    //bool RTE_Read_menu_switch_state_b;
-    uint8_t browse_menu_ctr_u8;
-    uint8_t main_menu_ctr_u8;
-    uint16_t main_menu_entry_exit_debounce_time_u16;
+    enum main_menu_et main_menu_e;                        // main menu options
+    enum browse_menu_et browse_menu_e;                    // browse menu options
+    enum menu_switch_state_et menu_switch_browse_state_e; // browse menu switch state(pressed/not pressed) 
+    bool main_menu_status_b;                              // true = inside main menu; false = otherwise
+    bool main_menu_entry_exit_b;                          // mani menu exit status
+    bool browse_menu_status_b;                            // true = inside browse menu; false = otherwise
+    uint8_t browse_menu_ctr_u8;                           // counter to go to the list of browse menu options
+    uint8_t main_menu_ctr_u8;                             // counter to go to the list of main menu options
+    uint16_t main_menu_entry_exit_debounce_time_u16;      // main menu debounce time
 
 private:
 
-    /*
-     */
+    /*!
+        @function
+        @abstract Main menu debounce time check
+        @discussion If main menu switch is pressed, the debounce time will start to increment.
+        And if pressed for at least 3sec., it will enter in main menu.
+
+        @param  none
+        @return none
+    */
     void menuLF_checkMainMenuDebounceTime(void);
 
-    /*
-     */
+    /*!
+        @function
+        @abstract Menu switch status check
+        @discussion Checks whether menu switch is pressed or not.
+
+        @param  none
+        @return none
+    */
     void menuLF_checkMenuSwitchStatus(void);
 
-    /*
-     */
+    /*!
+        @function
+        @abstract Browse menu
+        @discussion Every pressed and not pressed of the switch, the browse menu will go from one
+        option to another.
+
+        @param  none
+        @return none
+    */
     void menuLF_browseMenu(void);
 
-    /*
-     */
-    void menuLF_mainMenu(void);
+    /*!
+        @function
+        @abstract Main menu
+        @discussion Every pressed and not pressed of the switch, the main menu will go from one
+        option to another. Controller settings can be done here.
 
-    /*
-     */
+        @param  none
+        @return none
+    */
+    //void menuLF_mainMenu(void);
+
+    /*!
+        @function
+        @abstract Update Rte Information
+        @discussion Rte data will be updated here to make the information available to other modules.
+
+        @param  none
+        @return none
+    */
     void menuLF_updateRteData(void);
 }; // class RE_Menu_cls
 
